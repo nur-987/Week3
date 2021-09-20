f4310554b1325de64610abbe8ba18c85cce47c2d
nur-987 <76473637+nur-987@users.noreply.github.com>
2021-09-20 22:45:55 +0800

    Add project files.

---
+++ PasswordChecker/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input in password");
+            string myPass = Console.ReadLine();
+            Program myProg = new Program();
+            myProg.Checker(myPass);
+
+        }
+
+        public void Checker(string myPass)
+        {
+            for (int i = 0; i < myPass.Length - 1; i++)
+            {
+                if (myPass[i] == myPass[i + 1])
+                {
+                    Console.WriteLine("Passsword Should not have same Consecutive charcters");
+                    Console.ReadLine();
+                    return;
+                }
+
+            }
+
+            if (myPass.Length > 6)
+            {
+                if (myPass.Length < 24)
+                {
+                    char[] specialChar = {'!', '@', '#','$','%', '^', '&', '*','(',')', '+', '=', '_', '-', '{', '}', '[', ']',
+                                        ';', ':', '"','?', '<', '>', ',', '.' };
+                    foreach (char c in specialChar)
+                    {
+                        if (myPass.Contains(c))
+                        {
+                            if (myPass.Any(char.IsUpper))
+                            {
+                                if (myPass.Any(char.IsLower))
+                                {
+                                    if (myPass.Any(char.IsDigit))
+                                    {
+                                        Console.WriteLine("Checking.....");
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Password needs to have a digit");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Password has to have lower casing");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Password needs an Upper case");
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Password must have less than 24 characters");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Password has to be more than 6 characters");
+            }
+
+            Console.WriteLine("PASS all checks!");
+            Console.ReadLine();
+
+        }
+    }
+
+
+}
